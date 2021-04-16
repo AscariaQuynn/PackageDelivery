2457e23a19c8eae2c4cbfd8d13671451fd1fbbc7
Jaroslav Svoboda <jaroslav.svoboda@bsc-ideas.com>
2021-04-16 12:04:37 +0200

    Some minor modifications

--- src/main/java/cz/ascariaquynn/packagedelivery/Application.java
+++ src/main/java/cz/ascariaquynn/packagedelivery/Application.java
@@ -1,14 +1,14 @@
 package cz.ascariaquynn.packagedelivery;
 
-import cz.ascariaquynn.packagedelivery.controllers.UiController;
-import cz.ascariaquynn.packagedelivery.services.KillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import cz.ascariaquynn.packagedelivery.controllers.UiController;
+import cz.ascariaquynn.packagedelivery.services.KillService;
 
 @EnableAsync
 @SpringBootApplication
@@ -16,17 +16,16 @@ public class Application implements ApplicationRunner {
 
     private static KillService killService;
 
-    @Autowired
     private UiController uiController;
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     /**
      * Convenient method to shutdown without the need to inject KillService everywhere.
      * Also i think in this case, KillEvent would be unnecessary overkill.
-     * @param message
+     * @param message message to show on application quit
      */
     public static void exit(String message) {
         killService.exit(message);
@@ -36,6 +35,11 @@ public class Application implements ApplicationRunner {
     public void setKillService(KillService killService) {
         // Unfortunately static one, but i think is not too evil this time
         Application.killService = killService;
+    }
+
+    @Autowired
+    public void setUiController(UiController uiController) {
+        this.uiController = uiController;
     }
 
     @Override
